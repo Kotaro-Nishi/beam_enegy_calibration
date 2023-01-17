1919a08d63b35a1dd3f33e565a8f53b9020debab
Kotaro-Nishi <115140602+Kotaro-Nishi@users.noreply.github.com>
2023-01-17 13:15:50 +0900

    graph

---
+++ nishi/script_overview.py
@@ -0,0 +1,28 @@
+import graphviz
+
+g = graphviz.Digraph(format='png', filename='GUI')
+
+g.edge("MoFu","Eval_Modular")
+g.edge("AUndu","IFunc")
+g.edge("UnDi","ModularEvaluation")
+g.edge("Ifunc","ModularEvaluation")
+g.edge("AUndu","ModularEvaluation")
+g.edge("UnDi","MoFu")
+g.edge("IFunc","MoFu")
+g.edge("AUndu","MoFu")
+g.edge("UnDi","plot_modular_results")
+g.edge("IFunc","UnDi")
+g.edge("AUndu","UnDi")
+
+g.render(directory="nishi",view =True)
+
+h = graphviz.Digraph(format='png',filename ='NON_GUI')
+
+h.edge("AUndu","IFunc")
+h.edge("UnDi","ModularEvaluation_NonGUI")
+h.edge("IFunc","ModularEvaluation_NonGUI")
+h.edge("AUndu","ModularEvaluation_NonGUI")
+h.edge("AUndu","plot")
+h.edge("UnDi","plot")
+
+h.render(directory="nishi",view =True)
